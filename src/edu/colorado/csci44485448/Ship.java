eb37f7f38a6cd4469b5afd92475663a6fe32df73
[JustinVelvick] <[justin.velvick@colorado.edu]>
2021-02-01 15:04:01 -0700

    First commit

--- src/edu/colorado/csci44485448/Ship.java
+++ src/edu/colorado/csci44485448/Ship.java
@@ -3,10 +3,11 @@ package edu.colorado.csci44485448;
 // TODO: practice good OO design
 public class Ship {
     private String name;
+    private String todo;
     // TODO: create appropriate getter and setter methods
     // TODO: Understand encapsulation
     // TODO: Understand what these todo comments mean
-
+    //hello there, general kenobi! sdfsdfdsf dsfdsfsdf d
     public  void show()     {     // dunno why this is here maybe it is just an example method
         System.out.println("IF you can't see this then something is severely wrong!!");
     }
