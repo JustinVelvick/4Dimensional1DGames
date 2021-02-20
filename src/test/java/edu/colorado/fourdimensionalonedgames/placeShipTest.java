9947ed0270244393d9c09f8a65dcfda7cf7b6dad
hainsworth99 <31012794+hainsworth99@users.noreply.github.com>
2021-02-20 15:16:43 -0700

    wrote overlapping ship placement tests

--- src/test/java/edu/colorado/fourdimensionalonedgames/placeShipTest.java
+++ src/test/java/edu/colorado/fourdimensionalonedgames/placeShipTest.java
@@ -11,6 +11,7 @@ import java.awt.*;
 class placeShipTest {
 
     Ship testShip;
+    Ship testShip2;
     Board testBoard;
     static int tileSize = 40;
     static int rows = 10;
@@ -22,6 +23,7 @@ class placeShipTest {
         renderer = new Render();
         testBoard = new Board(columns, rows, renderer);
         testShip = new Destroyer();
+        testShip2 = new Minesweeper();
     }
 
     @Test
@@ -34,6 +36,38 @@ class placeShipTest {
         assertTrue(testBoard.placeShip(direction, origin, testShip));
     }
 
+    @Test
+    void placeInvalidShip(){
+        // goes off top of board
+        Orientation direction = Orientation.up;
+        Point2D origin = new Point2D(1,1);
+        assertFalse(testBoard.placeShip(direction, origin, testShip));
 
+        // goes off left side of board
+        direction = Orientation.left;
+        assertFalse(testBoard.placeShip(direction, origin, testShip));
+
+        // goes off bottom of board
+        direction = Orientation.down;
+        origin = new Point2D(10, 10);
+        assertFalse(testBoard.placeShip(direction, origin, testShip));
+
+        // goes off right side of board
+        direction = Orientation.right;
+        assertFalse(testBoard.placeShip(direction, origin, testShip));
+    }
+
+    @Test
+    void placeOverlappingShips(){
+        // place first ship
+        Orientation ship1Dir = Orientation.right;
+        Point2D ship1Origin = new Point2D(1,1);
+        assertTrue(testBoard.placeShip(ship1Dir, ship1Origin, testShip));
+
+        // place second, overlapping ship
+        Orientation ship2Dir = Orientation.up;
+        Point2D ship2Origin = new Point2D(2, 2);
+        assertFalse(testBoard.placeShip(ship2Dir, ship2Origin, testShip2));
+    }
 
 }
