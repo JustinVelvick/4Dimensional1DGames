87dd9fc49b0762b06f110810b102cd1ad2b3a43b
[JustinVelvick] <[justin.velvick@colorado.edu]>
2021-04-02 12:49:10 -0600

    Moving over observer pattern code from a really old branch part 3

---
+++ src/main/java/edu/colorado/fourdimensionalonedgames/render/gui/Subject.java
@@ -0,0 +1,8 @@
+package edu.colorado.fourdimensionalonedgames.render.gui;
+
+//boards
+public interface Subject {
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void updateObservers();
+}
